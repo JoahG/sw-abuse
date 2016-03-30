f8ef9527fda7e32531f215b8e6509190d65c811c
Cash Williams <CashWilliams@gmail.com>
2016-03-30 13:04:27 -0500

    Almost working

--- sw.js
+++ sw.js
@@ -12,16 +12,20 @@ this.addEventListener('install', function(event) {
 });
 
 this.addEventListener('fetch', function(event) {
-  var response;
-  event.respondWith(function() {
-    return fetch(event.request).then(function(r) {
-      response = r;
-      caches.open('v1').then(function(cache) {
-        console.log('caching response');
-        caches.put(event.request, response);
-      });
-      console.log('returning cloned response');
-      return response.clone();
-    })
+  fetch(event.request)
+  .then(function(response) {
+
+    function sleep(milliseconds) {
+      var start = new Date().getTime();
+      for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds){
+          break;
+        }
+      }
+    }
+
+    sleep(5000);
+    console.log("Sleeping :)");
+    return response;
   });
 });
