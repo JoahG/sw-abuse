f9ea70fd4eacc0cc3938faf7479705c6b7408076
Cash Williams <CashWilliams@gmail.com>
2016-03-30 11:08:14 -0500

    Trying to hijack a response

--- sw.js
+++ sw.js
@@ -12,12 +12,11 @@ this.addEventListener('install', function(event) {
 });
 
 this.addEventListener('fetch', function(event) {
-  var response;
   event.respondWith(
     return fetch(event.request).then(function(response) {
       return caches.open('v1').then(function(cache) {
         cache.put(event.request, response.clone());
-        return response;
+        return new Response('test is the response');
       })
     })
   );
